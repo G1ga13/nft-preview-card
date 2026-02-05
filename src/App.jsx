3f458a2d50762439ec3d1b3083d13811b8791929
Giga <gigashekelashvili1@gmail.com>
2026-02-05 14:39:09 +0400

    Feat:  added picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import nft from "./assets/images/nft.png";
 
-function Header() {
-  return <div className="main_container"></div>;
+function App() {
+  return (
+    <div className="main_container">
+      <div className="nft_image">
+        <img src={nft} alt="nft" />
+      </div>
+      <div className="container_text">
+        <h1>Equilibrium #3429</h1>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Header;
+export default App;
